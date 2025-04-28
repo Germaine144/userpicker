b3d5b5b97065a41a7caf69040687ffecdff5f65c
Germaine144 <umuhiregermaine12@gmail.com>
2025-04-28 09:46:19 +0200

    project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/FavoriteUserContext';
+
+function App() {
+  return (
+    <div className="App">
+     <FavoriteUserContext />
+    </div>
+  );
+}
+
+export default App;
